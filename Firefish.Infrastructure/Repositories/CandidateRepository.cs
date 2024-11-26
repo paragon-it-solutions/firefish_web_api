bd3d3ab62f9259f349ff15d017aec02241571dc3
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 01:16:14 +0000

    Refactored paramaterisation of sql command

--- Firefish.Infrastructure/Repositories/CandidateRepository.cs
+++ Firefish.Infrastructure/Repositories/CandidateRepository.cs
@@ -7,80 +7,9 @@ namespace Firefish.Infrastructure.Repositories;
 
 public class CandidateRepository : ICandidateRepository
 {
+    // Base query for retrieving all candidates from the database - can be modified to include additional filters or sorting as needed.
     private const string AllCandidateBaseQuery =
         "SELECT [ID], [FirstName], [Surname], [DateOfBirth], [Address1], [Town], [Country], [PostCode], [PhoneHome], [PhoneMobile], [PhoneWork], [CreatedDate], [UpdatedDate] FROM [Candidate]";
-
-    /// <summary>
-    ///     Retrieves a candidate from the database by their unique identifier.
-    /// </summary>
-    /// <param name="candidateId">The unique identifier of the candidate to retrieve.</param>
-    /// <returns>
-    ///     A Task that represents the asynchronous operation. The task result contains:
-    ///     - A Candidate object if a matching record is found.
-    ///     - Null if no matching record is found.
-    /// </returns>
-    /// <remarks>
-    ///     This method establishes a database connection, executes a SELECT query with the provided candidateId,
-    ///     and constructs a Candidate object from the retrieved data. It handles null values for optional fields.
-    /// </remarks>
-    public async Task<Candidate?> GetCandidateById(int candidateId)
-    {
-        await using var connection = new SqlConnection(SqlConnectionHelper.ConnectionString);
-        connection.Open();
-
-        await using var command = new SqlCommand(
-            $"{AllCandidateBaseQuery} WHERE {CandidateFieldNames.Id} = @{CandidateFieldNames.Id}",
-            connection
-        );
-        command.Parameters.AddWithValue("@Id", candidateId);
-
-        await using SqlDataReader? reader = await command.ExecuteReaderAsync();
-        if (reader.Read())
-        {
-            return new Candidate
-            {
-                Id = reader.GetInt32(reader.GetOrdinal(CandidateFieldNames.Id)),
-                FirstName = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.FirstName))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.FirstName)),
-                Surname = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Surname))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Surname)),
-                DateOfBirth = reader.GetDateTime(
-                    reader.GetOrdinal(CandidateFieldNames.DateOfBirth)
-                ),
-                Address = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Address))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Address)),
-                Town = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Town))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Town)),
-                Country = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Country))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Country)),
-                PostCode = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PostCode))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PostCode)),
-                PhoneHome = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneHome))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneHome)),
-                PhoneMobile = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneMobile))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneMobile)),
-                PhoneWork = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneWork))
-                    ? null
-                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneWork)),
-                CreatedDate = reader.GetDateTime(
-                    reader.GetOrdinal(CandidateFieldNames.CreatedDate)
-                ),
-                UpdatedDate = reader.GetDateTime(
-                    reader.GetOrdinal(CandidateFieldNames.UpdatedDate)
-                ),
-            };
-        }
-
-        return null;
-    }
 
     /// <summary>
     ///     Retrieves all candidates from the database.
@@ -154,11 +83,83 @@ public class CandidateRepository : ICandidateRepository
         return candidates;
     }
 
+    /// <summary>
+    ///     Retrieves a candidate from the database by their unique identifier.
+    /// </summary>
+    /// <param name="candidateId">The unique identifier of the candidate to retrieve.</param>
+    /// <returns>
+    ///     A Task that represents the asynchronous operation. The task result contains:
+    ///     - A Candidate object if a matching record is found.
+    ///     - Null if no matching record is found.
+    /// </returns>
+    /// <remarks>
+    ///     This method establishes a database connection, executes a SELECT query with the provided candidateId,
+    ///     and constructs a Candidate object from the retrieved data. It handles null values for optional fields.
+    /// </remarks>
+    public async Task<Candidate?> GetCandidateById(int candidateId)
+    {
+        await using var connection = new SqlConnection(SqlConnectionHelper.ConnectionString);
+        connection.Open();
+
+        await using var command = new SqlCommand(
+            $"{AllCandidateBaseQuery} WHERE {CandidateFieldNames.Id} = @{CandidateFieldNames.Id}",
+            connection
+        );
+        command.Parameters.AddWithValue("@Id", candidateId);
+
+        await using SqlDataReader? reader = await command.ExecuteReaderAsync();
+        if (reader.Read())
+        {
+            return new Candidate
+            {
+                Id = reader.GetInt32(reader.GetOrdinal(CandidateFieldNames.Id)),
+                FirstName = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.FirstName))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.FirstName)),
+                Surname = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Surname))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Surname)),
+                DateOfBirth = reader.GetDateTime(
+                    reader.GetOrdinal(CandidateFieldNames.DateOfBirth)
+                ),
+                Address = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Address))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Address)),
+                Town = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Town))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Town)),
+                Country = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.Country))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.Country)),
+                PostCode = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PostCode))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PostCode)),
+                PhoneHome = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneHome))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneHome)),
+                PhoneMobile = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneMobile))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneMobile)),
+                PhoneWork = reader.IsDBNull(reader.GetOrdinal(CandidateFieldNames.PhoneWork))
+                    ? null
+                    : reader.GetString(reader.GetOrdinal(CandidateFieldNames.PhoneWork)),
+                CreatedDate = reader.GetDateTime(
+                    reader.GetOrdinal(CandidateFieldNames.CreatedDate)
+                ),
+                UpdatedDate = reader.GetDateTime(
+                    reader.GetOrdinal(CandidateFieldNames.UpdatedDate)
+                ),
+            };
+        }
+
+        return null;
+    }
+
     public async Task<Candidate> CreateCandidate(Candidate candidate)
     {
         await using var connection = new SqlConnection(SqlConnectionHelper.ConnectionString);
         connection.Open();
-        
+
         // "SELECT SCOPE_IDENTITY()" returns the identity value of the inserted row.
         await using var command = new SqlCommand(
             """
@@ -192,49 +193,13 @@ public class CandidateRepository : ICandidateRepository
             """,
             connection
         );
-        command.Parameters.AddWithValue($"@{CandidateFieldNames.Id}", candidate.Id);
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.FirstName}",
-            candidate.FirstName
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.Surname}",
-            candidate.Surname
-        );
-        command.Parameters.AddWithValue(
-            $"@{CandidateFieldNames.DateOfBirth}",
-            candidate.DateOfBirth
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.Address}",
-            candidate.Address
-        );
-        command.Parameters.AddWithNullableValue($"@{CandidateFieldNames.Town}", candidate.Town);
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.Country}",
-            candidate.Country
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.PostCode}",
-            candidate.PostCode
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.PhoneHome}",
-            candidate.PhoneHome
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.PhoneMobile}",
-            candidate.PhoneMobile
-        );
-        command.Parameters.AddWithNullableValue(
-            $"@{CandidateFieldNames.PhoneWork}",
-            candidate.PhoneWork
-        );
-        command.Parameters.AddWithValue($"@{CandidateFieldNames.UpdatedDate}", DateTime.Now);
+        
+        command.Parameters.AddWithValue($"@{CandidateFieldNames.CreatedDate}", DateTime.Now);
+        ParameteriseValuesForCommand(command, candidate);
 
         // Sets ID to db identity column
         candidate.Id = (int)await command.ExecuteScalarAsync();
-        
+
         return candidate;
     }
 
@@ -272,7 +237,18 @@ public class CandidateRepository : ICandidateRepository
             """,
             connection
         );
+        
+        // Parameterise the command with candidate's values'
         command.Parameters.AddWithValue($"@{CandidateFieldNames.Id}", candidate.Id);
+        ParameteriseValuesForCommand(command, candidate);
+        
+        await command.ExecuteNonQueryAsync();
+    }
+
+    // Private static method to parameterise SQL command with candidate's value's - void as parameters collection will be modified on original reference object
+    // Note: Id and CreatedDate are not included here as they vary across update and create operations
+    private static void ParameteriseValuesForCommand(SqlCommand command, Candidate candidate)
+    {
         command.Parameters.AddWithNullableValue(
             $"@{CandidateFieldNames.FirstName}",
             candidate.FirstName
@@ -311,7 +287,5 @@ public class CandidateRepository : ICandidateRepository
             candidate.PhoneWork
         );
         command.Parameters.AddWithValue($"@{CandidateFieldNames.UpdatedDate}", DateTime.Now);
-
-        await command.ExecuteNonQueryAsync();
     }
 }
