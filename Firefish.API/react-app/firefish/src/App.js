2581b37728d582b10b47b3db64e74dc6f551b23b
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 21:27:16 +0000

    set dark theme default and imported material ui

--- Firefish.API/react-app/firefish/src/App.js
+++ Firefish.API/react-app/firefish/src/App.js
@@ -1,6 +1,7 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import CandidatesList from './components/candidates/CandidatesList';
 import AddCandidate from './components/candidates/AddCandidate';
 import EditCandidate from './components/candidates/EditCandidate';
@@ -8,17 +9,26 @@ import EditCandidate from './components/candidates/EditCandidate';
 // import AddSkill from './components/AddSkill';
 // import RemoveSkill from './components/RemoveSkill';
 
+// Create a dark theme
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={CandidatesList} />
-          <Route path="/add" component={AddCandidate} />
-          <Route path="/edit/:id" component={EditCandidate} />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<CandidatesList />} />
+            <Route path="/add" element={<AddCandidate />} />
+            <Route path="/edit/:id" element={<EditCandidate />} />
+          </Routes>
+        </div>  
+      </Router>
+    </ThemeProvider>
   );
 }
 
