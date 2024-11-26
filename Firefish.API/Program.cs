9071bd3eedad4d248cc2141226fa1a0bd348c9e8
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 02:00:06 +0000

    Used dependency injection to inject candidate service and repo

--- Firefish.API/Program.cs
+++ Firefish.API/Program.cs
@@ -1,11 +1,24 @@
+using Firefish.Core.Contracts.Repositories;
+using Firefish.Core.Contracts.Services;
+using Firefish.Core.Entities;
+using Firefish.Infrastructure.Repositories;
+using Firefish.Infrastructure.Services;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configure controllers and endpoint routing.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+// Configure Repositories
+builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
+
+// Configure Services
+builder.Services.AddScoped<ICandidateService, CandidateService>();
 
 // CORS configuration
 builder.Services.AddCors(options =>
