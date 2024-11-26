6faf283b52325c0ea87d8c6cb4620e7bab9db632
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 17:32:03 +0000

    renamed for clarity

--- Firefish.Core/Models/Skill/Requests/CandidateSkillRequestModel.cs
+++ Firefish.Core/Models/Skill/Requests/CandidateSkillRequestModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Firefish.Core.Models.Skill.Requests;
 
-public class SkillRequestModel
+public class CandidateSkillRequestModel
 {
     public required int CandidateId { get; set; }
     public required int SkillId { get; set; }
