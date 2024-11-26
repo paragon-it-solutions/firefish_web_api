d450481df9b23e7234be19d58d265ea09e1c0115
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 19:42:29 +0000

    Renamed method to make clearer

--- Firefish.Core/Contracts/Repositories/ISkillRepository.cs
+++ Firefish.Core/Contracts/Repositories/ISkillRepository.cs
@@ -7,6 +7,6 @@ public interface ISkillRepository
     Task<IEnumerable<CandidateSkill>> GetSkillsByCandidateIdAsync(int candidateId);
     Task<IEnumerable<CandidateSkill>> AddSkillByCandidateIdAsync(int candidateId, int skillId);
     Task<IEnumerable<CandidateSkill>> RemoveSkillByIdAsync(int candidateSkillId);
-    Task<bool> SkillExistsAsync(int skillId);
+    Task<bool> CandidateSkillExists(int skillId);
     Task<bool> SkillExistsForCandidateAsync(int skillId, int candidateId);
 }
