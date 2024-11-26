f263e288ea423b592350fd81e1182720b4b8078e
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 00:36:51 +0000

    Added interface for candidate repository

---
+++ Firefish.Core/Contracts/Repositories/ICandidateRepository.cs
@@ -0,0 +1,11 @@
+﻿using Firefish.Core.Entities;
+
+namespace Firefish.Core.Contracts.Repositories;
+
+public interface ICandidateRepository
+{
+    Task<Candidate?> GetCandidateById(int candidateId);
+    Task<List<Candidate>> GetAllCandidates();
+    Task<Candidate> CreateCandidate(Candidate candidate);
+    Task UpdateExistingCandidate(Candidate candidate);
+}
