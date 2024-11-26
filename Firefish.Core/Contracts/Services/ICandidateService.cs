b743cbd5cc2f72a96b162961c6d09a36dea2aef5
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-26 01:17:15 +0000

    Added interface for CandidateService.cs implementation

---
+++ Firefish.Core/Contracts/Services/ICandidateService.cs
@@ -0,0 +1,12 @@
+﻿using Firefish.Core.Models.Candidate.Requests;
+using Firefish.Core.Models.Candidate.Responses;
+
+namespace Firefish.Core.Contracts.Services;
+
+public interface ICandidateService
+{
+    Task<List<CandidateListItemResponseModel>> GetAllCandidates();
+    Task<CandidateDetailsResponseModel> GetCandidateById(int candidateId);
+    Task<CandidateDetailsResponseModel> CreateCandidate(CandidateCreateOrUpdateRequestModel candidate);
+    Task UpdateExistingCandidate(CandidateCreateOrUpdateRequestModel candidate);
+}
