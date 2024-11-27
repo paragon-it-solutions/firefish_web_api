df428305780e1d5d7532f40e975269f35cd2636e
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-27 16:27:02 +0000

    renamed to clarify CandidateSkillResponseModel.cs from SKillReponseModel

---
+++ Firefish.Core/Models/Skill/Responses/CandidateSkillResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Firefish.Core.Models.Skill.Responses;
+
+public class CandidateSkillResponseModel
+{
+    public int CandidateSkillId { get; set; }
+    public required int SkillId { get; set; }
+    public required string Name { get; set; }
+}
