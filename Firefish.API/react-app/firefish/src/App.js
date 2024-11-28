9ecf8694bc02c5570a9975351dd801ee8486f196
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-28 02:47:55 +0000

    formatted code

--- Firefish.API/react-app/firefish/src/App.js
+++ Firefish.API/react-app/firefish/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { teal } from '@mui/material/colors';
-import CssBaseline from '@mui/material/CssBaseline';
-import CandidatesList from './components/candidates/CandidatesList';
-import AddCandidate from './components/candidates/AddCandidate';
-import EditCandidate from './components/candidates/EditCandidate';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { teal } from "@mui/material/colors";
+import CssBaseline from "@mui/material/CssBaseline";
+import CandidatesList from "./components/candidates/CandidatesList";
+import AddCandidate from "./components/candidates/AddCandidate";
+import EditCandidate from "./components/candidates/EditCandidate";
 // import SkillsList from './components/SkillsList';
 // import AddSkill from './components/AddSkill';
 // import RemoveSkill from './components/RemoveSkill';
@@ -13,11 +13,11 @@ import EditCandidate from './components/candidates/EditCandidate';
 // Create a dark theme
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: teal,
     secondary: {
-      main: '#ff7043',
-    },    
+      main: "#ff7043",
+    },
   },
 });
 function App() {
@@ -31,7 +31,7 @@ function App() {
             <Route path="/add" element={<AddCandidate />} />
             <Route path="/edit/:id" element={<EditCandidate />} />
           </Routes>
-        </div>  
+        </div>
       </Router>
     </ThemeProvider>
   );
