6b245d82a8853d9b6aa9b656c784e93f30a6086b
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-28 04:42:14 +0000

    removed unused imports and updated routing

--- Firefish.API/react-app/firefish/src/App.js
+++ Firefish.API/react-app/firefish/src/App.js
@@ -4,12 +4,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { teal } from "@mui/material/colors";
 import CssBaseline from "@mui/material/CssBaseline";
 import CandidatesList from "./components/candidates/CandidatesList";
-import AddCandidate from "./components/candidates/AddCandidate";
-import EditCandidate from "./components/candidates/EditCandidate";
-// import SkillsList from './components/SkillsList';
-// import AddSkill from './components/AddSkill';
-// import RemoveSkill from './components/RemoveSkill';
-
+import AddCandidateForm from "./components/candidates/AddCandidateForm";
+import EditCandidateForm from "./components/candidates/EditCandidate";
 // Create a dark theme
 const darkTheme = createTheme({
   palette: {
@@ -28,8 +24,8 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/" element={<CandidatesList />} />
-            <Route path="/add" element={<AddCandidate />} />
-            <Route path="/edit/:id" element={<EditCandidate />} />
+            <Route path="/add" element={<AddCandidateForm />} />
+            <Route path="/edit/:candidateId" element={<EditCandidateForm />} />
           </Routes>
         </div>
       </Router>
